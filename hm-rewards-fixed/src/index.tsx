a0f01faecf77b448ee9cebb1b6993a833270b8d4
hygieneematic-commits <hygieneematic@gmail.com>
2026-05-19 16:05:58 +0530

    Add files via upload

---
+++ hm-rewards-fixed/src/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import ReactDOM from "react-dom/client";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./main.css";
+
+import App from "./App";
+
+import AdminPanel from "./AdminPanel";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/admin" element={<AdminPanel />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
