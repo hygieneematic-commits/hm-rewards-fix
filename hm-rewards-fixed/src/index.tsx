e5982fa967bbdcbbaa2c06febed3dcd319c0359d
hygieneematic-commits <hygieneematic@gmail.com>
2026-05-21 00:08:51 +0530

    Update index.tsx

--- hm-rewards-fixed/src/index.tsx
+++ hm-rewards-fixed/src/index.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-
 import ReactDOM from "react-dom/client";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./main.css";
 
 import App from "./App";
-
 import AdminPanel from "./AdminPanel";
 
 const root = ReactDOM.createRoot(
